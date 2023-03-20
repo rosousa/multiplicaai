14cc14dcb9743da1d44290523c504982ad8930bf
rosousa <rodolfozsousa@gmail.com>
2023-03-20 19:32:39 -0300

    added scanner and show multiplication table

--- src/MultiplicaAi.java
+++ src/MultiplicaAi.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class MultiplicaAi {
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Digite um número e aperte enter!");
 
+        int number = userInput.nextInt();
+
+        System.out.println("Tabela de multiplicação de " + number);
+
+        final int MAX = 10;
+
+        for(int i = 1; i <= MAX; i++) {
+            System.out.println(i + " * " + number + " = " + (i * number));
+        }
     }
 }
